77f189c98e37e373707312385604d487e6cd8602
John Lombard <john.lombard1996@gmail.com>
2019-10-27 11:24:36 -0400

    More practice

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print(" World")
+print(" Hello")
+
+
+cars = 100
+
+print("there are", cars, "cars")
---
+++ question.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+age = input()
+
+print(f"I am {age} ")
+
+
+height = input("How tall are you?")
+weight = input("How much do you weight?")
+
+print(f"You are {height} tall and weigh {weight}")
